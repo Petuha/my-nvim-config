11ef0dc551dff32adaf43009122d2bfda67ebfd3
Petr <ptikhonoff@gmail.com>
2026-01-15 23:39:55 +0300

    shortcuts ctrl + Backspace, ctrl + Delete, ctrl + C

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,14 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- map("i", "jk", "<ESC>")
+
+map("i", "<C-H>", "<C-w>", { desc = "Delete previous word with Ctrl + Backspace" })
+map("n", "<C-H>", "a<C-w><ESC>", { desc = "Delete previous word with Ctrl + Backspace" })
+
+map("i", "<C-Del>", "<C-o>dw", { desc = "Delete next word with Ctrl + Delete" })
+map("n", "<C-Del>", "dw", { desc = "Delete next word with Ctrl + Delete" })
+
+map("n", "<C-c>", "a", { desc = "Enter INSERT mode from NORMAL" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
