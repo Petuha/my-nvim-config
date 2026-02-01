4ca3bb7cff3bd2ba1551a04e334dc7ece9bfdb32
Petr <ptikhonoff@gmail.com>
2026-02-01 13:00:38 +0300

    enh: less langmapper shortcuts in help

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,6 +20,33 @@ return {
       require("langmapper").setup({})
     end,
   },
+  {
+    "folke/which-key.nvim",
+    opts = function(_, opts)
+      local translate_key = require("langmapper.utils").translate_keycode
+      -- don't show mappings translated by langmapper.nvim. Show entry if func returns true
+      opts.filter = function(mapping)
+        return mapping.lhs
+          and mapping.lhs == translate_key(mapping.lhs, "default", "ru")
+          and mapping.desc
+          and mapping.desc:find("LM") == nil
+      end
+    end,
+  },
+  {
+    "folke/snacks.nvim",
+    optional = true,
+    opts = function(_, _)
+      local translate_key = require("langmapper.utils").translate_keycode
+      local normkey_orig = Snacks.util.normkey
+      Snacks.util.normkey = function(key)
+        if key then
+          key = translate_key(key, "default", "ru")
+        end
+        return normkey_orig(key)
+      end
+    end,
+  },
 
   {
     'Civitasv/cmake-tools.nvim',
